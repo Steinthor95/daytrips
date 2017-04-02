4306869de95b108b0e1cdea0f9ff7be79849b4f3
Steinthor95 <Steinthor_Ardal@hotmail.com>
2017-04-02 17:43:10 +0000

    Changed getData() function, stopped using Object[][] and started using String[][]

--- DataManager.java
+++ DataManager.java
@@ -6,30 +6,13 @@ public class DataManager{
 
     public String[][] readData(){
         String[] lines = new In("trips.txt").readAllLines();
-        String[][] splitline = new String[lines.length][13];
+        String[][] data = new String[lines.length][13];
         String[] line = new String[13];
         for(int i = 0; i < lines.length; i++){
             for(int j = 0; j < 13; j++){
                 line = lines[i].split(";");
-                splitline[i][j] = line[j];
+                data[i][j] = line[j];
             }
-        }
-
-        String[][] data = new String[lines.length][13];
-        for(int i = 0; i < lines.length; i++){
-            data[i][0] = splitline[i][0];
-            data[i][1] = splitline[i][1];
-            data[i][2] = splitline[i][2];
-            data[i][3] = splitline[i][3];
-            data[i][4] = splitline[i][4];
-            data[i][5] = splitline[i][5];
-            data[i][6] = splitline[i][6];
-            data[i][7] = splitline[i][7];
-            data[i][8] = splitline[i][8];
-            data[i][9] = splitline[i][9];
-            data[i][10] = splitline[i][10];
-            data[i][11] = splitline[i][11];
-            data[i][12] = splitline[i][12];
         }
         return data;
     }
